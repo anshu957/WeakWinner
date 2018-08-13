eb24f80ecbd19b717a08612db71560d418a56f9a
anshu957 <anshu957@anshul.choudhary@uol.de>
2018-08-13 02:02:27 +0200

    updated combinations part

--- utils/Combinations.h
+++ utils/Combinations.h
@@ -3,6 +3,10 @@
 #include <algorithm>
 #include <assert.h>
 
+// Used from the stack-overflow answer by the user Jarod42  in the following url
+// https://stackoverflow.com/questions/28711797/generating-n-choose-k-permutations-in-c
+
+// Basically it takes in a vector and size of subset and writes all the combinations of subsets into a 2d vector
 template <typename T>
 void Combination(const std::vector<T> &v, std::size_t count, std::vector<std::vector<T>> &v2)
 {
